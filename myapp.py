88af04e93da611b890e01d2dc39179a09c218c0c
teresacerdan <99200929+teresacerdan@users.noreply.github.com>
2022-04-20 21:00:30 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,8 +1,6 @@
 import streamlit as st
-import panda as pd
+import pandas as pd
 import numpy as np
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
 
-image = Image.open('KDT-JU.png')
-st.image(image)
 st.title("My first web app")

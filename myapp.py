92d1b34d7e5343bf0848598652f34d078f9c1885
teresacerdan <99200929+teresacerdan@users.noreply.github.com>
2022-04-20 21:05:53 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -7,3 +7,9 @@ chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
 st.title("My first web app")
 image = Image.open('KDT-JU.png')
 st.image(image)
+
+st.dataframe(chart_data)
+@st.cache
+
+option = st.selectbox(['a','b','c'])
+st.bar_chart(option)

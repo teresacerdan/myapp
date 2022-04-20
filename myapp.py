a5ec771a852e392b70aa81e0fb1d7b9762c0f962
teresacerdan <99200929+teresacerdan@users.noreply.github.com>
2022-04-20 21:02:11 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from PIL import Image
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
 
 st.title("My first web app")
+image = Image.open('KDT-JU.png')
+st.image(image)

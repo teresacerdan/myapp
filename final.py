2f9131093f1cb125fcf01d9b01d3f8d4527d2418
teresacerdan <99200929+teresacerdan@users.noreply.github.com>
2022-04-29 20:49:27 +0200

    Update final.py

--- final.py
+++ final.py
@@ -1,3 +1,13 @@
+import sqlite3
+import pandas as pd
+from sqlite3 import connect
+
+conn = sqlite3.connect('db1.db')
+cur = conn.cursor()
+cur.execute('SELECT Countries.Country FROM Countries')
+df = pd.DataFrame(cur.fetchall(), columns = ['Countries'])
+
+
 import streamlit as st
 import pandas as pd
 countries = []

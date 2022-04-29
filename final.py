fa5a0f84565acca3f4b0aaf0b16abcdcfcb62bdc
teresacerdan <99200929+teresacerdan@users.noreply.github.com>
2022-04-29 21:14:28 +0200

    Update final.py

--- final.py
+++ final.py
@@ -4,8 +4,8 @@ from sqlite3 import connect
 
 conn = sqlite3.connect('db1.db')
 cur = conn.cursor()
-cur.execute('SELECT Countries.Country FROM Countries')
-df = pd.DataFrame(cur.fetchall(), columns = ['Countries'])
+cur.execute('SELECT Country, Acronym FROM Countries')
+df = pd.DataFrame(cur.fetchall(), columns = ['Countries', 'Acronym'])
 
 
 import streamlit as st
@@ -15,4 +15,4 @@ countries = []
 for i in acronym:
   countries.append(i)
 
-country_acronym = st.radio('Country acronym', countries)
+country_acronym = st.selectbox('Country acronym', countries)

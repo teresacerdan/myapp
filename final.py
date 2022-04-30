fa34e7bd14d2508721d4a7c9b3d1deb706fbea83
teresacerdan <99200929+teresacerdan@users.noreply.github.com>
2022-04-30 20:43:02 +0200

    Update final.py

--- final.py
+++ final.py
@@ -17,6 +17,6 @@ for i in acronym:
 country_selected = st.selectbox('Country name', countries)
 
 
-cur.execute("SELECT shortName, name, activityType, ecContribution, organizationURL, COUNT(organizationURL) FROM Participants WHERE role = 'coordinator', country=country_selected GROUP BY organizationURL ORDER BY ecContribution DESC")
+cur.execute("DECLARE @country AS VARCHAR() = country_selected SELECT shortName, name, activityType, ecContribution, organizationURL, COUNT(organizationURL) FROM Participants WHERE role = 'coordinator', country=country_selected GROUP BY organizationURL ORDER BY ecContribution DESC")
 df_participants = pd.DataFrame(cur.fetchall(), columns= ['shortName', 'name', 'activityType', 'Sum','organizationURL', 'count_project'])
 st.dataframe(df_participants)

b589442c37f498671de219f45965fff24909bc46
teresacerdan <99200929+teresacerdan@users.noreply.github.com>
2022-05-06 20:59:24 +0200

    Update final.py

--- final.py
+++ final.py
@@ -19,7 +19,7 @@ st.write('You selected:', country_selected)
 
 acronym_selected=pd.read_sql("SELECT Acronym FROM Countries WHERE Country='country_selected'", conn) 
                              
-c1 = pd.read_sql("SELECT country, shortName, name, activityType, ecContribution, organizationURL, COUNT(organizationURL) FROM Participants WHERE country = 'acronym_selected'", conn)
+c1 = pd.read_sql("SELECT country, shortName, name, activityType, ecContribution, organizationURL, COUNT(organizationURL) FROM Participants", conn)
 df_participants = pd.DataFrame(c1, columns= ['country', 'shortName', 'name', 'activityType', 'Sum','organizationURL', 'count_project'])
 conn.close()
 st.dataframe(df_participants)

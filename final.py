55bd5221fe2106ba19ae7395397969e5a619cf6d
teresacerdan <99200929+teresacerdan@users.noreply.github.com>
2022-05-06 18:46:28 +0200

    Update final.py

--- final.py
+++ final.py
@@ -10,11 +10,12 @@ df = pd.DataFrame(cur.fetchall(), columns = ['Countries', 'Acronym'])
 
 import streamlit as st
 import pandas as pd
-acronym = df['Countries']
+countries_column = df['Countries']
 countries = []
 for i in acronym:
   countries.append(i)
 country_selected = st.selectbox('Country name', countries)
+st.write('You selected:', country_selected)
 
 cur.execute("SELECT shortName, name, activityType, ecContribution, organizationURL, COUNT(organizationURL) FROM Participants WHERE role = 'coordinator' GROUP BY organizationURL ORDER BY ecContribution DESC")
 df_participants = pd.DataFrame(cur.fetchall(), columns= ['shortName', 'name', 'activityType', 'Sum','organizationURL', 'count_project'])

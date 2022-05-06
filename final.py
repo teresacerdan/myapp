ca990b566f3596624312f1f21a6902c023fbc811
teresacerdan <99200929+teresacerdan@users.noreply.github.com>
2022-05-06 19:46:43 +0200

    Update final.py

--- final.py
+++ final.py
@@ -18,5 +18,5 @@ country_selected = st.selectbox('Country name', countries)
 st.write('You selected:', country_selected)
 
 cur.execute("SELECT shortName, name, activityType, ecContribution, organizationURL COUNT(organizationURL) FROM Participants WHERE role = 'coordinator' GROUP BY organizationURL ORDER BY ecContribution DESC")
-df_participants = pd.DataFrame(cur.fetchall(), columns= ['shortName', 'name', 'activityType', 'Sum','organizationURL', 'count_project'])
+df_participants = pd.DataFrame(cur.fetchall(), columns= (['shortName', 'name', 'activityType', 'Sum','organizationURL', 'count_project'])
 st.dataframe(df_participants)

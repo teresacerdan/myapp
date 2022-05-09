2499f3f15f66fe78c39996dd9e9dcc7a192cc1e5
teresacerdan <99200929+teresacerdan@users.noreply.github.com>
2022-05-09 16:23:06 +0200

    Update final.py

--- final.py
+++ final.py
@@ -14,9 +14,12 @@ from PIL import Image
 countries_column = df['Countries']
 countries = []
 
+#Title and image
 image=Image.open('KDT-JU.png')
 st.image(image)
-st.header('Partner search tool')
+st.title('Partner search tool')
+
+#Selectbox with list of countries (association with the acronym)
 for i in countries_column:
   countries.append(i)
 country_selected = st.selectbox('Country name', countries)
@@ -31,11 +34,10 @@ for i in acronym_column:
   acronym.append(i)
 my_acronym=acronym[position]
 
-
 st.write('You selected:', country_selected,',',my_acronym)
 
-
-st.write('Participants of', country_selected)
+#Dataframe of participants
+st.header('Participants of', country_selected)
 participants=pd.read_sql("SELECT country, shortName, name, activityType, SUM(ecContribution), organizationURL, COUNT(organizationURL) FROM Participants WHERE role = 'participant' AND country = '{}' GROUP BY organizationURL ORDER BY SUM(ecContribution)DESC".format(my_acronym),conn)
 df_participants = pd.DataFrame(participants, columns= ['country', 'shortName', 'name', 'activityType', 'Sum','organizationURL', 'count_project'])  
 #appplying background color to df
@@ -45,8 +47,8 @@ df_participants = df_participants.style.set_properties(**{'background-color': 'l
                                                     'border-color': 'white'})
 st.dataframe(df_participants)
 
-             
-st.write('Coordinators of', country_selected)
+#Dataframe of coordinators
+st.header('Coordinators of', country_selected)
 coordinators=pd.read_sql("SELECT shortName, name, ActivityType, projectAcronym FROM participants WHERE role='coordinator' AND country='{}'ORDER BY shortName".format(my_acronym),conn)
 df_coordinators = pd.DataFrame(coordinators, columns= ['Short Name', 'Name', 'Activity Type', 'Project Acronym'])  
 st.dataframe(df_coordinators)

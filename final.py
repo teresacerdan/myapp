2957762b94b4901152d9e86e008769df57ab214b
teresacerdan <99200929+teresacerdan@users.noreply.github.com>
2022-05-09 15:43:34 +0200

    Update final.py

--- final.py
+++ final.py
@@ -54,8 +54,7 @@ headers = {
     'props': [("background-color", "orange"), ("color", "green"),
               ("border", "3px solid black")]
 }
-df_participants = df_participants.style.
-                                        set_table_styles([cell_hover, index_names, headers])
+df_participants = df_participants.style.set_table_styles([cell_hover, index_names, headers])
 df_participants
 st.dataframe(df_participants)
 

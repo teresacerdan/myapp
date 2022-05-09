7ebfded588ad5d6a44cff08199cb2bb6db800d93
teresacerdan <99200929+teresacerdan@users.noreply.github.com>
2022-05-09 12:45:18 +0200

    Update final.py

--- final.py
+++ final.py
@@ -10,15 +10,41 @@ df = pd.DataFrame(cur.fetchall(), columns = ['Countries', 'Acronym'])
 
 import streamlit as st
 import pandas as pd
+from PIL import Image
 countries_column = df['Countries']
 countries = []
+
+image=Image.open('WhatsApp Image 2022-05-08 at 8.26.19 PM.jpeg')
+st.image(image)
+st.header('Partner search tool')
 for i in countries_column:
   countries.append(i)
 country_selected = st.selectbox('Country name', countries)
-st.write('You selected:', country_selected)
 
-acronym_selected=pd.read_sql("SELECT Acronym FROM Countries WHERE Country=country_selected", conn) 
-                             
-c1=pd.read_sql("SELECT country, shortName, name, activityType, ecContribution, organizationURL, COUNT(organizationURL) FROM Participants WHERE role = 'coordinator' AND country=acronym_selected GROUP BY organizationURL ORDER BY ecContribution DESC",conn)
-df_participants = pd.DataFrame(c1, columns= ['country', 'shortName', 'name', 'activityType', 'Sum','organizationURL', 'count'])
+for i in range(len(countries)):
+  if countries[i]==country_selected:
+    position=i
+  
+acronym_column = df['Acronym']
+acronym=[]
+for i in acronym_column:
+  acronym.append(i)
+my_acronym=acronym[position]
+
+
+st.write('You selected:', country_selected,',',my_acronym)
+
+
+
+st.write('Participants of', country_selected)
+participants=pd.read_sql("SELECT country, shortName, name, activityType, ecContribution, organizationURL, SUM(ecContribution) AS sum, COUNT(organizationURL) FROM participants WHERE role='participant' AND country='{}' GROUP BY organizationURL ORDER BY sum DESC".format(my_acronym),conn)
+df_participants = pd.DataFrame(participants, columns= ['country', 'shortName', 'name', 'activityType', 'Sum','organizationURL', 'count'])  
 st.dataframe(df_participants)
+
+st.write('Coordinators of', country_selected)
+coordinators=pd.read_sql("SELECT shortName, name, ActivityType, projectAcronym FROM participants WHERE role='coordinator' AND country='{}'ORDER BY shortName".format(my_acronym),conn)
+df_coordinators = pd.DataFrame(coordinators, columns= ['Short Name', 'Name', 'Activity Type', 'Project Acronym'])  
+st.dataframe(df_coordinators)
+
+
+
